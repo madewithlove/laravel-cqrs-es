8badd49f6a5c7e7c90cba2be21c10ea9773a6e70
Jonas Drieghe <jonasd@madewithlove.be>
2016-04-22 15:37:36 +0200

    remove obsolete config vars

--- config/broadway.php
+++ config/broadway.php
@@ -5,32 +5,16 @@ return [
     |--------------------------------------------------------------------------
     | Event Store configuration
     |--------------------------------------------------------------------------
-    | Set the table name where the event will be stored. Make sure
-    | this corresponds to what you specified in the migration
-    |
-    | You should also choose a driver, possible options are
+    | You can choose a driver, possible options are:
+    | 
     | dbal, inmemory
+    |
     */
     'event-store' => [
         'driver' => 'dbal',
         'dbal' => [
             'connection' => 'mysql',
             'table' => 'event_store',
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Choose which read model implementation to use
-    | Possible options are: elasticsearch, inmemory
-    |--------------------------------------------------------------------------
-    */
-    'read-model' => 'elasticsearch',
-    'read-model-connections' => [
-        'elasticsearch' => [
-            'config' => [
-                'hosts' => ['localhost:9200'],
-            ],
         ],
     ],
 ];
