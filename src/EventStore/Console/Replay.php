84bb7870a66fdda6ddb671038f3a6b0298a384ca
Xavier Barbosa <xavier@madewithlove.be>
2020-03-06 20:02:28 +0000

    Update method name

--- src/EventStore/Console/Replay.php
+++ src/EventStore/Console/Replay.php
@@ -59,10 +59,10 @@ class Replay extends Command
             $params['types'] = explode(',', $types);
         }
 
-        $this->dispatcher->fire(new PreEventStoreReplay());
+        $this->dispatcher->dispatch(new PreEventStoreReplay());
 
         $this->replayService->replay($params);
 
-        $this->dispatcher->fire(new PostEventStoreReplay());
+        $this->dispatcher->dispatch(new PostEventStoreReplay());
     }
 }
